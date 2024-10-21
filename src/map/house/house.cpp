5a2e0607c50d400f293c2305231f0c92cb7292e1
leo36dff <its.notover@icloud.com>
2024-10-21 16:44:52 -0300

    update house
    house

--- src/map/house/house.cpp
+++ src/map/house/house.cpp
@@ -154,7 +154,7 @@ void House::updateDoorDescription() const {
 	const int32_t housePrice = getPrice();
 	if (housePrice != -1) {
 		if (g_configManager().getBoolean(HOUSE_PURSHASED_SHOW_PRICE) || owner == 0) {
-			ss << " It costs " << formatNumber(getPrice()) << "tibia coins.";
+			ss << " It costs " << formatNumber(getPrice()) << " tibia coins.";
 		}
 		std::string strRentPeriod = asLowerCaseString(g_configManager().getString(HOUSE_RENT_PERIOD));
 		if (strRentPeriod != "never") {
